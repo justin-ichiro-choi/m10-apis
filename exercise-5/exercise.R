05f6a408f5d091e286ce1416178bb9cb157753c9
Justin Choi <justinch@uw.edu>
2016-10-27 15:03:06 -0700

    Welp

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -3,13 +3,15 @@ library(jsonlite)
 library(dplyr)
 
 # Write a function that allows you to specify a movie, then does the following:
-  
+makeData <- function(movie) {
   # Replace all of the spaces in your movie title with plus signs (+)
-  
+  movie.no.spaces <- gsub(" ", "+",movie)
   
   # Construct a search query using YOUR api key
   # The base URL is https://api.nytimes.com/svc/movies/v2/reviews/search.json?
   # See the interactive console for more detail:https://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json
+  base_URL <- "https://api.nytimes.com/svc/movies/v2/reviews/search.json?"
+  api_key <- "7853cdc19a884a03affe196ce1e1c037"
   
   
   # Request data using your search query
@@ -24,7 +26,7 @@ library(dplyr)
   
   
   # Return an list of the three pieces of information from above
-  
-
+  return(movie.no.spaces)
+}
 # Test that your function works with a movie of your choice
-
+makeData("The Godfather Part 2")
